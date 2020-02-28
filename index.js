726dedf625f8b3d44c41806adb35c0ad7fa85bfb
Gary Anderson <ganderson@seattleacademy.org>
2020-02-28 10:41:03 -0800

    Add attendance feature

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 1500
+const attendance = [];
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) => {
+	let person = req.query.name;
+	attendance.push(person);
+	console.log(attendance)
+	res.send(attendance)
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
